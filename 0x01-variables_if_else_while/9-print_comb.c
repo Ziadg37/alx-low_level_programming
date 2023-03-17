13aa2b965b3649364b9e7d33ecf9f652846d46e4
com <ziadxx3gf@gmil.com>
2023-03-17 13:25:49 -0700

    10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-int main(void) {
-    // loop over all possible digits
-    for (int i = 0; i < 10; i++) {
-        // print the current digit
-        putchar('0' + i);
-        
-        // if this is not the last digit, print the separator
-        if (i < 9) {
-            putchar(',');
-            putchar(' ');
-            
-            // loop over the remaining digits
-            for (int j = i + 1; j < 10; j++) {
-                // print the current digit
-                putchar('0' + j);
-                
-                // if this is not the last digit, print the separator
-                if (j < 9) {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    
-    return 0;
-}
+/**
+ * main - prints all possible combinations of single digit numbers.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
 
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
